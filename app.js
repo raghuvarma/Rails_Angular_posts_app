f6249141791798ff386c4169add18193635a23bd
Raghu Varma R <rrv461@gmail.com>
2016-01-30 12:12:40 +0530

    rrvApp: added posts with title and upvotes as votes for each post

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 angular.module('rrvApp', [])
 .controller('MainCtrl', ['$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.appHeading = 'Welcome to RRV Application';
+  $scope.posts = [
+  {title: 'post 1', upvotes: 5},
+  {title: 'post 2', upvotes: 2},
+  {title: 'post 3', upvotes: 15},
+  {title: 'post 4', upvotes: 9},
+  {title: 'post 5', upvotes: 4}
+];
 }]);
