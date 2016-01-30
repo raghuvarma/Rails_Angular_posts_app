6b57837c05460cc93c56b0df68973bddf4c11e0b
Raghu Varma R <rrv461@gmail.com>
2016-01-30 14:39:00 +0530

    rrvApp: added routes, added comments for posts, added routes for home and posts using inline templates and added postsCtrl for comments

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-angular.module('rrvApp', [])
-.factory('posts', [function(){
+angular.module('rrvApp', ['ui.router'])
+.config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		});
+	}
+])
+  $urlRouterProvider.otherwise('home');
+.factory('posts', [ function(){
   var o = {
     posts: []
   };
@@ -28,6 +45,29 @@ angular.module('rrvApp', [])
 		};
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
+		};
+	}
+])
+.controller('PostsCtrl', ['$scope', '$stateParams', 'posts',
+	function($scope, $stateParams, posts){
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0,
+			comments: [
+				{author: 'Raghu', body: 'Cool post!', upvotes: 0},
+				{author: 'Varma', body: 'Great idea but everything is wrong!', upvotes: 0}
+			]
+		});
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
 		};
 	}
 ]);
